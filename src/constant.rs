e8f0dae16230b29715a4e4be3df26a880eb38865
poly000 <34085039+poly000@users.noreply.github.com>
2022-05-21 05:10:09 +0800

    Update constant.rs

--- src/constant.rs
+++ src/constant.rs
@@ -2,8 +2,17 @@ pub mod offset {
     pub const SAVE_LOCATION: isize = 0x08;
     pub const CURRENT_HEALTH: isize = 0x20;
     pub const MAX_HEALTH: isize = 0x1C;
+    pub const WEAPON_SIZE: isize = 0x14;
+    pub const WEAPON_TYPE: isize = 0x38;
+    pub const WEAPON_LEVEL: isize = 0x3C;
+    pub const WEAPON_EXP: isize = 0x40;
+    pub const WEAPON_MAX_AMMO: isize = 0x44;
+    pub const WEAPON_CURRENT_AMMO: isize = 0x48;
+    pub const INVENTORY_TYPE: isize = 0xd8;
+    pub const DIFFICULTY: isize = 0x610; // Cave Story+, 0 for normal, 2 for easy, 4 for hard
 }
 
 pub mod limitation {
-    pub const INVENTORY_SLOT: i32 = 24;
+    pub const INVENTORY_SLOT: i32 = 24; // each inventory is of 0x4 bytes
+    pub const WEAPON_SLOT: i32 = 6;
 }
