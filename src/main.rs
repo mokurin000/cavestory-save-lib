6ab8f0bfa453f0b571a8a486ac72b227765b1538
poly000 <1348292515@qq.com>
2022-05-21 17:59:53 +0800

    remove addtional spaces

--- src/main.rs
+++ src/main.rs
@@ -24,24 +24,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .prompt()?;
         match option {
             "Health" => {
-                profile.set_current_health(type_int_value("current: ", profile.current_health())?);
-                profile.set_max_health(type_int_value("max: ", profile.max_health())?)
+                profile.set_current_health(type_int_value("current:", profile.current_health())?);
+                profile.set_max_health(type_int_value("max:", profile.max_health())?)
             }
             "Weapon" => {
                 let slot = type_int_value("slot to edit:", 0)? as isize;
 
                 profile.set_weapon_type(select_id(&WEAPON, profile.weapon_type(slot))?, slot);
 
-                let level = type_int_value("level: ", profile.weapon_level(slot))?;
+                let level = type_int_value("level:", profile.weapon_level(slot))?;
                 profile.set_weapon_level(level, slot);
 
-                let exp_level = type_int_value("exp level: ", profile.weapon_exp(slot))?;
+                let exp_level = type_int_value("exp level:", profile.weapon_exp(slot))?;
                 profile.set_weapon_exp(exp_level, slot);
 
-                let ammo = type_int_value("ammo: ", profile.weapon_ammo(slot))?;
+                let ammo = type_int_value("ammo:", profile.weapon_ammo(slot))?;
                 profile.set_weapon_ammo(ammo, slot);
 
-                let max_ammo = type_int_value("max ammo: ", profile.weapon_max_ammo(slot))?;
+                let max_ammo = type_int_value("max ammo:", profile.weapon_max_ammo(slot))?;
                 profile.set_weapon_max_ammo(max_ammo, slot);
             }
             "Inventory" => {
@@ -71,7 +71,7 @@ fn type_int_value(tip: &str, default: i32) -> Result<i32, Box<dyn std::error::Er
 }
 
 fn select_id(list: &[&str], default: i32) -> Result<i32, Box<dyn std::error::Error>> {
-    let opt = Select::new("select the new value: ", list.to_vec()).prompt_skippable()?;
+    let opt = Select::new("select the new value:", list.to_vec()).prompt_skippable()?;
     if let Some(option) = opt {
         Ok(list
             .iter()
