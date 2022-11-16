53c459720ed0f4915ae807896ec230c82a677507
poly000 <1348292515@qq.com>
2022-11-16 20:10:39 +0800

    update

--- src/profile/mod.rs
+++ src/profile/mod.rs
@@ -1,5 +1,5 @@
 mod offset;
-
+use offset::*;
 
 /// entire data from `profile.dat`
 #[derive(Clone, Debug)]
@@ -63,77 +63,77 @@ impl From<Profile> for Vec<u8> {
 
 impl Profile {
     pub fn set_health(&mut self, health: i16) {
-        self.edit16(offset::HEALTH, health);
+        self.edit16(HEALTH, health);
     }
 
     pub fn set_max_health(&mut self, max_health: i16) {
-        self.edit16(offset::MAX_HEALTH, max_health);
+        self.edit16(MAX_HEALTH, max_health);
     }
 
     pub fn set_weapon_type(&mut self, weapon: i32, slot: usize) {
-        self.edit(offset::WEAPON_TYPE + offset::WEAPON_SIZE * slot, weapon);
+        self.edit(FIRST_WEAPON_TYPE + WEAPON_SIZE * slot, weapon);
     }
 
     pub fn set_weapon_level(&mut self, level: i32, slot: usize) {
-        self.edit(offset::WEAPON_LEVEL + offset::WEAPON_SIZE * slot, level);
+        self.edit(FIRST_WEAPON_LEVEL + WEAPON_SIZE * slot, level);
     }
 
     pub fn set_weapon_exp(&mut self, level: i32, slot: usize) {
-        self.edit(offset::WEAPON_EXP + offset::WEAPON_SIZE * slot, level);
+        self.edit(FIRST_WEAPON_EXP + WEAPON_SIZE * slot, level);
     }
 
     pub fn set_weapon_ammo(&mut self, ammo: i32, slot: usize) {
-        self.edit(offset::WEAPON_AMMO + offset::WEAPON_SIZE * slot, ammo);
+        self.edit(FIRST_WEAPON_AMMO + WEAPON_SIZE * slot, ammo);
     }
 
     pub fn set_weapon_max_ammo(&mut self, max_ammo: i32, slot: usize) {
         self.edit(
-            offset::WEAPON_MAX_AMMO + offset::WEAPON_SIZE * slot,
+            FIRST_WEAPON_MAX_AMMO + WEAPON_SIZE * slot,
             max_ammo,
         );
     }
 
     pub fn set_inventory(&mut self, inventory: i32, slot: usize) {
-        self.edit(offset::INVENTORY_TYPE + 4 * slot, inventory)
+        self.edit(FIRST_INVENTORY_TYPE + INVENTORY_SIZE * slot, inventory)
     }
 
     pub fn set_music(&mut self, song: i32) {
-        self.edit(offset::SONG, song);
+        self.edit(SONG, song);
     }
 
     pub fn health(&self) -> i16 {
-        self.read16(offset::HEALTH)
+        self.read16(HEALTH)
     }
 
     pub fn max_health(&self) -> i16 {
-        self.read16(offset::MAX_HEALTH)
+        self.read16(MAX_HEALTH)
     }
 
     pub fn weapon_type(&self, slot: usize) -> i32 {
-        self.read(offset::WEAPON_TYPE + offset::WEAPON_SIZE * slot)
+        self.read(FIRST_WEAPON_TYPE + WEAPON_SIZE * slot)
     }
 
     pub fn weapon_level(&self, slot: usize) -> i32 {
-        self.read(offset::WEAPON_LEVEL + offset::WEAPON_SIZE * slot)
+        self.read(FIRST_WEAPON_LEVEL + WEAPON_SIZE * slot)
     }
 
     pub fn weapon_exp(&self, slot: usize) -> i32 {
-        self.read(offset::WEAPON_EXP + offset::WEAPON_SIZE * slot)
+        self.read(FIRST_WEAPON_EXP + WEAPON_SIZE * slot)
     }
 
     pub fn weapon_ammo(&self, slot: usize) -> i32 {
-        self.read(offset::WEAPON_AMMO + offset::WEAPON_SIZE * slot)
+        self.read(FIRST_WEAPON_AMMO + WEAPON_SIZE * slot)
     }
 
     pub fn weapon_max_ammo(&self, slot: usize) -> i32 {
-        self.read(offset::WEAPON_MAX_AMMO + offset::WEAPON_SIZE * slot)
+        self.read(FIRST_WEAPON_MAX_AMMO + WEAPON_SIZE * slot)
     }
 
     pub fn inventory(&self, slot: usize) -> i32 {
-        self.read(offset::INVENTORY_TYPE + 4 * slot)
+        self.read(FIRST_INVENTORY_TYPE + INVENTORY_SIZE * slot)
     }
 
     pub fn music(&self) -> i32 {
-        self.read(offset::SONG)
+        self.read(SONG)
     }
 }
--- src/profile/offset.rs
+++ src/profile/offset.rs
@@ -2,12 +2,13 @@ pub(super) const HEALTH: usize = 0x20;
 pub(super) const MAX_HEALTH: usize = 0x1C;
 
 pub(super) const WEAPON_SIZE: usize = 0x14;
-pub(super) const WEAPON_TYPE: usize = 0x38;
-pub(super) const WEAPON_LEVEL: usize = 0x3C;
-pub(super) const WEAPON_EXP: usize = 0x40;
-pub(super) const WEAPON_MAX_AMMO: usize = 0x44;
-pub(super) const WEAPON_AMMO: usize = 0x48;
+pub(super) const FIRST_WEAPON_TYPE: usize = 0x38;
+pub(super) const FIRST_WEAPON_LEVEL: usize = 0x3C;
+pub(super) const FIRST_WEAPON_EXP: usize = 0x40;
+pub(super) const FIRST_WEAPON_MAX_AMMO: usize = 0x44;
+pub(super) const FIRST_WEAPON_AMMO: usize = 0x48;
 
-pub(super) const INVENTORY_TYPE: usize = 0xd8;
+pub(super) const INVENTORY_SIZE: usize = 0x04;
+pub(super) const FIRST_INVENTORY_TYPE: usize = 0xd8;
 
 pub(super) const SONG: usize = 0x0C;
