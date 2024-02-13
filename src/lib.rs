ce9aad16d5a1b3170246fbca771f5e8232ed062d
poly000 <1348292515@qq.com>
2024-02-13 17:04:04 +0800

    fix: export strum

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 //! check [profile.txt](https://github.com/poly000/cavestory-save-lib/blob/master/profile.txt) for value validation.
 
-#![feature(pointer_byte_offsets)]
-
 /// Custom types to ensure data be valid
 pub mod items;
 
@@ -11,3 +9,6 @@ mod game_profile;
 pub use profile::Profile;
 pub use profile::ProfileError;
 pub use game_profile::GameProfile;
+
+#[cfg(feature = "strum")]
+pub use strum;
