ebca81d7367ca417ecc16f0e447c5f9ba87ca4ab
poly000 <1348292515@qq.com>
2024-03-24 18:12:44 +0800

    feat: from_unchecked

--- src/profile/mod.rs
+++ src/profile/mod.rs
@@ -18,63 +18,45 @@ pub enum ProfileError {
 
 impl Profile {
     fn edit32(&mut self, offset: usize, value: i32) {
-        if offset + 4 > self.0.len() {
-            panic!("out of bound");
-        }
-
-        let p = self.0.as_mut_ptr() as *mut [u8; 4];
-
-        unsafe {
-            p.byte_offset(offset as isize)
-                .write_unaligned(value.to_le_bytes())
-        };
+        self.0[offset..offset + 4].copy_from_slice(&value.to_le_bytes())
     }
 
     fn edit16(&mut self, offset: usize, value: i16) {
-        if offset + 2 > self.0.len() {
-            panic!("out of bound");
-        }
-
-        let p = self.0.as_mut_ptr() as *mut [u8; 2];
-
-        unsafe {
-            p.byte_offset(offset as isize)
-                .write_unaligned(value.to_le_bytes())
-        };
+        self.0[offset..offset + 2].copy_from_slice(&value.to_le_bytes())
     }
 
     fn read32(&self, offset: usize) -> i32 {
-        if offset + 4 > self.0.len() {
-            panic!("out of bound");
-        }
-
-        let bytes: [u8; 4] = unsafe {
-            (self.0.as_ptr() as *const [u8; 4])
-                .byte_offset(offset as _)
-                .read_unaligned()
-        };
+        let mut bytes: [u8; 4] = Default::default();
+        bytes.copy_from_slice(&self.0[offset..offset + 4]);
         i32::from_le_bytes(bytes)
     }
 
     fn read16(&self, offset: usize) -> i16 {
-        if offset + 2 > self.0.len() {
-            panic!("out of bound");
-        }
-
-        let bytes: [u8; 2] = unsafe {
-            (self.0.as_ptr() as *const [u8; 2])
-                .byte_offset(offset as _)
-                .read_unaligned()
-        };
+        let mut bytes: [u8; 2] = Default::default();
+        bytes.copy_from_slice(&self.0[offset..offset + 2]);
         i16::from_le_bytes(bytes)
     }
 
-    fn verify(&self) -> bool {
-        let p = self.0.as_ptr() as *const [u8; 8];
-        unsafe {
-            let head: [u8; 8] = p.read();
-            &head == b"Do041220"
+    fn verify_header(&self) -> bool {
+        let head = &self.0[..8];
+        &head == b"Do041220"
+    }
+
+    pub fn from_raw_without_length_check(data: Vec<u8>) -> Result<Self, ProfileError> {
+        let profile = Profile(data);
+
+        if profile.0.len() < 0x604 {
+            return Err(ProfileError::LengthNotMatch);
         }
+        if !profile.verify_header() {
+            return Err(ProfileError::IllegalFileHead);
+        }
+
+        Ok(profile)
+    }
+
+    pub fn from_raw_unchecked(data: Vec<u8>) -> Self {
+        Profile(data)
     }
 }
 
@@ -87,7 +69,7 @@ impl TryFrom<Vec<u8>> for Profile {
         if profile.0.len() != 0x604 {
             return Err(ProfileError::LengthNotMatch);
         }
-        if !profile.verify() {
+        if !profile.verify_header() {
             return Err(ProfileError::IllegalFileHead);
         }
 
